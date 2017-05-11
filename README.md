eec4403f0152d0f10d645b50b216011669b22956
Alexey Maslennikov <alexey@maslennikov.me>
2017-05-11 08:43:56 +0200

    more consistent command interface

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MUV
 
-Modern migration javascript toolkit with schema versioning.
+Migration toolkit for node.js with baselining and versioning integrity checks.
 
 ## Features
 
@@ -18,10 +18,47 @@ You should also install `knex` as it's a peer dependency of this package.
 
 ## Usage
 
-First, init project with `knex init`, add migrations with `knex migrate:make`, and then:
+First, init project with `knex init`, add migrations with `muv create`, and then:
 
 ```
-Usage: TBD
+Usage
+  $ muv <command> [options]
+
+Commands
+  status         Shows current schema-, baseline version, and pending migrations
+  log            Lists executed migrations since baseline (pass -v to show all)
+  baseline       Move baseline to a specified migration
+  up             Performs all pending migrations
+  down           Rollbacks last migration
+  make           Makes a migration with a given name in migrations directory
+
+Options for "up" and "down":
+  up --to, -t <name>      Migrate upto specific version
+  down --to, -t <name|0>  Migrate downto specific version or to baseline if 0
+  (up|down) --dry-run, d  Only show list of potential changes without exeuting
+
+Options for "baseline:
+  baseline                  Query current schema version and baseline
+  baseline --to, -t <name>  Move baseline to a specified version
+
+Options for "make:
+  make <name>  Make a timestamped migration file with a given name
+
+Global options:
+  --cwd          Specify the working directory
+  --knexfile     Specify the knexfile path ($cwd/knexfile.js)
+  --migrations   Specify migrations path ($cwd/migrations)
+  --env          Specify environment ($KNEX_ENV || $NODE_ENV || 'development')
+  --verbose, -v  Be more verbose
+
+Examples
+  $ muv status                # query current status
+  $ muv up                    # migrate everyting
+  $ muv up --to 20160905      # migrate to a given version
+  $ muv down --to 0           # rollback all migrations (downto baseline)
+  $ muv down                  # rollback single migration
+  $ muv baseline              # shows current version and baseline
+  $ muv baseline --to 201701  # sets baseline to a new version
 ```
 
 ## Thank you
--- src/cli.js
+++ src/cli.js
@@ -12,37 +12,40 @@ const cli = meow(
     $ muv <command> [options]
 
   Commands
-    schema-version Shows current schema and baseline version
-    log            Lists all executed migrations since baseline
+    status         Shows current schema-, baseline version, and pending migrations
+    log            Lists executed migrations since baseline (pass -v to show all)
     baseline       Move baseline to a specified migration
     up             Performs all pending migrations
     down           Rollbacks last migration
-    make           Makes a migration with a given name
+    make           Makes a migration with a given name in migrations directory
 
   Options for "up" and "down":
     up --to, -t <name>      Migrate upto specific version
     down --to, -t <name|0>  Migrate downto specific version or to baseline if 0
-    --dry-run, d            Only show list of potential changes without exeuting
+    (up|down) --dry-run, d  Only show list of potential changes without exeuting
 
   Options for "baseline:
-    baseline <name>  Move baseline to a specified version
+    baseline                  Query current schema version and baseline
+    baseline --to, -t <name>  Move baseline to a specified version
 
   Options for "make:
-    make <name>  Make a migration file with a given name in migrations dir
+    make <name>  Make a timestamped migration file with a given name
 
   Global options:
-    --cwd         Specify the working directory
-    --knexfile    Specify the knexfile path ($cwd/knexfile.js)
-    --migrations  Specify migrations path ($cwd/migrations)
-    --env         Specify environment ($KNEX_ENV || $NODE_ENV || 'development')
-    --verbose     Be more verbose
+    --cwd          Specify the working directory
+    --knexfile     Specify the knexfile path ($cwd/knexfile.js)
+    --migrations   Specify migrations path ($cwd/migrations)
+    --env          Specify environment ($KNEX_ENV || $NODE_ENV || 'development')
+    --verbose, -v  Be more verbose
 
   Examples
-    $ muv up                  # migrate everyting
-    $ muv up --to 20160905    # migrate to a given version
-    $ muv down --to 0         # rollback all migrations (downto baseline)
-    $ muv down                # rollback single migration
-    $ muv rollback            # rollback previous "up"
+    $ muv status                # query current status
+    $ muv up                    # migrate everyting
+    $ muv up --to 20160905      # migrate to a given version
+    $ muv down --to 0           # rollback all migrations (downto baseline)
+    $ muv down                  # rollback single migration
+    $ muv baseline              # shows current version and baseline
+    $ muv baseline --to 201701  # sets baseline to a new version
  `,
   {
     alias: {
@@ -87,17 +90,20 @@ async function main () {
   const command = cli.input[0]
 
   switch (command) {
-  case 'schema-version':
+  case 'status':
     assertArgs(!cli.input[1])
     await api.schemaVersion()
+    await api.pending()
     break
   case 'log':
     assertArgs(!cli.input[1])
     await api.executed()
     break
   case 'baseline':
-    assertArgs(cli.input[1])
-    await api.baseline(cli.input[1])
+    assertArgs(!cli.input[1])
+    if (cli.flags.to) {
+      await api.baseline(cli.flags.to)
+    }
     await api.schemaVersion()
     break
   case 'up':
@@ -138,7 +144,7 @@ function createApi (stdout, migrator) {
         migrator.currentVersion(),
         migrator.currentBaseline()
       ])
-      print(`Schema version ${v || '0'} ${b ? `(baseline ${b})` : ''}`)
+      print(`Schema version ${v || '0'} (${b ? `baseline ${b}` : 'no baseline'})`)
     },
 
     executed: async () => {
